de993d5349d4462e85828d6a4adc9665359ea314
Shivam Vishwakarma <106417521+ShivamVishw@users.noreply.github.com>
2026-03-23 02:12:42 +0530

    Update MissingNumberInN.java

--- Array Questions/MissingNumberInN.java
+++ Array Questions/MissingNumberInN.java
@@ -1,20 +1,17 @@
 public class MissingNumberInN {
     
-    static int missingNumber(int arr[], int N){
-        // Remember N is always startig from 1 
-        int tempHashArr[] = new int[N+1];
-        for(int i=0; i<arr.length; i++){
-            tempHashArr[arr[i]] = tempHashArr[arr[i]] + 1;
+     // Find the Missing Number
+    static int missingNumber(int arr[], int n){
+        // By using sum formula 
+        int total = (int) n * (n+1) /2;
+
+        int sum =0;
+        for(int val : arr){
+            sum +=val;
         }
 
-        for(int i=0; i<=tempHashArr.length; i++){   // start from zero for considering zero also 
-            if(tempHashArr[i]==0){
-                return i;
-            }
-        }
-       
-        return -1;
-    }
+        return total-sum;
+    }   
 
     public static void main(String[] args) {
         int arr[] = {9,6,4,2,3,5,7,0,1};
